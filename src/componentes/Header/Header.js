cc71490d399ef42073b4bde7059ce2929e4141b6
leonardo <leojn8@gmail.com>
2021-10-27 20:26:56 -0300

    atual

--- src/componentes/Header/Header.js
+++ src/componentes/Header/Header.js
@@ -1,17 +1,14 @@
 import styles from '../Header.module.css';
 
-
-export default function Header(){
-    return(
-        
-        
-        <div className={styles.containerHeader}>
-            
-            <div className={styles.textHeader}>
-                <h3>Saiba na hora o quanto seu aplicativo está te cobrando em <big>%</big>, o considerado certo pela UBER é de <big>25%</big></h3>
-            </div>
-            
-        </div>
-        
-    )
+export default function Header() {
+  return (
+    <div className={styles.containerHeader}>
+      <div className={styles.textHeader}>
+        <h3>
+          Saiba na hora o quanto seu aplicativo está te cobrando em <big>%</big>
+          , o considerado certo pela UBER é de <big>25%</big>
+        </h3>
+      </div>
+    </div>
+  );
 }
