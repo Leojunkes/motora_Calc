985daa9bff2123060b4f734ac509b520bd23fef4
leonardo <leojn8@gmail.com>
2021-11-02 23:08:15 -0300

    atual

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,20 @@
 import Footer from '../componentes/footer/footer'
 import Header from '../componentes/Header/Header'
+import {ChakraProvider} from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
   return(
+    
+    //<ChakraProvider>
     <>
     <Header/>
   <Component {...pageProps} />
   <Footer/>
   </>
+  //</ChakraProvider>
+  
   )
    
 }
